2fb31e401aa24513b621a12f2b5e2702634b1a3f
AhmadAlzean <ahmadalzean22@gmail.com>
2023-05-12 22:58:59 +0300

    Initiate project

---
+++ AdstrTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdstrTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
